794068d74d51b21cce614383003d554573987d19
RohanQWT <149373674+Rohanquanticteq@users.noreply.github.com>
2026-09-24 17:19:13 +0530

    error line uncommented in create todo controller

--- src/controller/todo-controller.ts
+++ src/controller/todo-controller.ts
@@ -6,13 +6,12 @@ export const TodoController = {
     res: Response,
   ): Promise<void> => {
     try {
-      console.log("HI", req.body);
       const newToDo = new Todo({ title: req.body.title });
       const result = await newToDo.save();
       res.status(201).json(result);
     } catch (error) {
       console.error("Error creating todo:", error);
-      // res.status(500).json({ error: 'Failed to create todo.', details: error });
+      res.status(500).json({ error: error });
     }
   },
 };
